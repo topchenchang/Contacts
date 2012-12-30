a92b735b8b48225fde3252322e0b8333b09591be
orange <topchenchang@gamil.com>
2012-12-30 21:30:16 +0800

    Update PhoneManage/src/com/orange/peoplemanage/AddPeople.java
    update in the github !

--- PhoneManage/src/com/orange/peoplemanage/AddPeople.java
+++ PhoneManage/src/com/orange/peoplemanage/AddPeople.java
@@ -5,6 +5,6 @@ public class AddPeople {
 
 	public void Insert(String name){
 //		ContentResolver resolver= getContext().getContentResolver();
-		
+//test the git , if i update in the github ,the cilent is update right.		
 	}
 }
